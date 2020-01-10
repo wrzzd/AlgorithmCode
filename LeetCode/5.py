f9e52ce4cb9dd23f6be74be6f3abb46f91303e00
wrzzd <wrzzd@hotmail.com>
2020-01-10 13:51:36 +0800

    5

---
+++ LeetCode/5.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def longestPalindrome(self, s):
+    	n = len(s)
+        dp = [[0] * n for _ in range(n)]
+        max_len = float("-inf")
+        res = ""
+        for i in range(n):
+            for j in range(i, -1, -1):
+                if s[i] == s[j] and (i - j < 2 or dp[i - 1][j + 1]):
+                    dp[i][j] = 1
+
+                if dp[i][j] and i - j + 1 > max_len:
+           
+                    max_len = i - j + 1
+                    res = s[j:i + 1]
+        return res
